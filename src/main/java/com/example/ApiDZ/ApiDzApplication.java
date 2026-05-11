a9fcc877c0d3c5149ed74c79c050fac5802abbef
BB <bbislv12@gmail.com>
2026-05-11 16:24:25 +0300

    Initial commit

---
+++ src/main/java/com/example/ApiDZ/ApiDzApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiDZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDzApplication.class, args);
+	}
+
+}
